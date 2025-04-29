0cf63ded6ac24017f42542bd35dad805d698d620
Orest Yakymiv <akimivorest@gmail.com>
2025-04-29 15:39:56 +0300

    create statefulset

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+cd .infrastructure
+kubectl apply -f secret.yml
+kubectl apply -f st-secret.yml
+kubectl apply -f st-configMap.yml
+kubectl apply -f st-service.yml
+kubectl apply -f statefulSet.yml
+kubectl apply -f deployment.yml
